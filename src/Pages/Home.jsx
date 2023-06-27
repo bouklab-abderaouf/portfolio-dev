1c121b23e37b067e613667c0c41a38dde174734c
Raouf <abderaouf-hocine.bouklab@epitech.eu>
2023-06-27 16:53:17 +0200

    [ADD] Hero section
    Added Home page with personal introduction and logo

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import logo  from '../assets/logo.png'
 
 export const Home = () => {
   return (
-    <div className=''>
-          <h1>This is the home page</h1>
+    <div className=' w-full h-screen flex flex-row justify-between'>
+      <div className='flex flex-col justify-center w-1/2'>
+      <p className='text-[#D7E5EC] text-xl font-medium font-sora'>FRONT WEB DEVELOPER</p>
+      <h1 className='text-6xl font-montserrat bg-clip-text text-transparent bg-gradient-to-r from-[#00F5A0] to-[#00D9F5] font-semibold '>Bouklab Abderaouf</h1>
+      <p className='text-[#BDEBEA] text-base font-medium	w-full'>Hello, I'm Abderaouf Bouklab, a Front-End Developer and a Master's student at EPITECH Paris, specializing in software architecture and development. I'm currently seeking a one-year internship in the field of computer science.
+</p>
+      </div>
+      <div className='flex flex-col justify-center '>
+        <img src={logo} className='w-[400px] h-[300px]' />
+      
 
-    
+      </div>
     </div>
   )
 }
