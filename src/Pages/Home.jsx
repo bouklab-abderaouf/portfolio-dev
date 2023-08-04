11a0c6c4723edb64558508d51ad7ca7e8c5e8203
Raouf <abderaouf-hocine.bouklab@epitech.eu>
2023-08-04 12:55:24 +0200

    [fix] about me section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -14,9 +14,9 @@ export const Home = () => {
       <div className='flex flex-col justify-center items-center sm:items-start w-full sm:w-1/2 mt-8 sm:mt-0'>
       <p className='text-[#D7E5EC] sm:text-xl text-l font-medium font-sora'>FRONT WEB DEVELOPER</p>
       <h1 className='text-2xl sm:text-5xl font-montserrat bg-clip-text text-transparent bg-gradient-to-r from-[#00F5A0] to-[#00D9F5] font-semibold '>Bouklab Abderaouf</h1>
-      <p className='text-[#BDEBEA] text-base font-medium	w-full'>Hello, I'm Abderaouf Bouklab, a Front-End Developer and a Master's student at EPITECH Paris, specializing in software architecture and development. I'm currently seeking a one-year internship in the field of computer science.
-      </p>
-      <NavLink  className='inline-block py-3 px-5 mt-4 text-[#252728] rounded-md bg-gradient-to-r from-[#00F5A0] to-[#00D9F5] ' to='/About'>Contact Me</NavLink>
+      {/* <p className='text-[#BDEBEA] text-base font-medium	w-full'>Hello, I'm Abderaouf Bouklab, a Front-End Developer and a Master's student at EPITECH Paris, specializing in software architecture and development. I'm currently seeking a one-year internship in the field of computer science.
+      </p> */}
+      {/* <NavLink  className='inline-block py-3 px-5 mt-4 text-[#252728] rounded-md bg-gradient-to-r from-[#00F5A0] to-[#00D9F5] ' to='/About'>Contact Me</NavLink> */}
       </div>
       <div className='flex flex-col justify-center items-end w-full sm:w-1/2 '>
         <img src={logo} alt='logo' className='w-[300px] h-[200px] sm:w-[400px] sm:h-[300px]' />
@@ -27,9 +27,7 @@ export const Home = () => {
       <div className='flex flex-col sm:flex-row justify-between items-center'>
         <div className='flex flex-col justify-center items-center w-full '>
         <p className='text-[#BDEBEA] text-base font-medium	w-full'>
-          Hello! My name is Abderaouf Bouklab and I am passionate about creating innovative solutions on the web. My journey into web development began during my Master's degree at the University of Badji Mokhtar Annaba, where I specialized in Systems and Decisions. This experience taught me a lot about responsive design, database architecture, and workflow optimization.
-
-          Fast-forward to today, I am currently pursuing my M1 at EPITECH Paris, focusing on software architecture and development. I have had the privilege of working as a full stack bubble.io developer at Netdevices Paris, where I honed my skills in front-end development and gained a comprehensive understanding of full-stack development.
+          Hello! My name is Abderaouf Bouklab and I am passionate about creating innovative solutions on the web. I am currently pursuing my M2 at EPITECH Paris, focusing on software architecture and development. I have had the privilege of working as a full stack bubble.io developer at Netdevices Paris, where I honed my skills in front-end development and gained a comprehensive understanding of full-stack development.
           I am currently seeking a one-year internship in the field of computer science, where I can leverage my skills and experience to create innovative solutions.
         </p>
         </div>
