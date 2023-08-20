91a8612cf07327c9bb5a9f080722f84ae2a6ad07
Raouf <abderaouf-hocine.bouklab@epitech.eu>
2023-08-20 17:59:34 +0200

    [ADD] socials component
    with on click mailto function

---
+++ src/components/Socials.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import data from '../Socials.json'
+
+const handleEmailClick = () => {
+  window.location.href = 'mailto:abderaouf-hocine.bouklab@epitech.eu';
+}
+
+const Socials = () => {
+  return (
+    <div className='flex flex-row '>
+    {data.map( (items) =>  (
+    <div key={items.name}>
+    { items.name === "Gmail" ?  <img src= {items.imgSrc} onClick={handleEmailClick} width={'50px'} height={'50px'} />
+    :
+    <a href={items.Link} target="_blank" rel="noopener noreferrer" >
+    <img src= {items.imgSrc} width={'50px'} height={'50px'} />
+    </a>
+    }
+    </div>
+    ))}
+    </div>
+  )
+}
+
+export default Socials
