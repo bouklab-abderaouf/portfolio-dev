56c4359f858d26f7e2d4c5e1248611e7a7e21392
Raouf <abderaouf-hocine.bouklab@epitech.eu>
2023-08-21 10:49:31 +0200

    netlify Build fixes

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -10,10 +10,10 @@ const Socials = () => {
     <div className='flex flex-row '>
     {data.map( (items) =>  (
     <div key={items.name}>
-    { items.name === "Gmail" ?  <img src= {items.imgSrc} onClick={handleEmailClick} width={'50px'} height={'50px'} />
+    { items.name === "Gmail" ?  <img src= {items.imgSrc} alt={items.alt} onClick={handleEmailClick} width={'50px'} height={'50px'} />
     :
     <a href={items.Link} target="_blank" rel="noopener noreferrer" >
-    <img src= {items.imgSrc} width={'50px'} height={'50px'} />
+    <img src= {items.imgSrc} alt={items.alt} width={'50px'} height={'50px'} />
     </a>
     }
     </div>
